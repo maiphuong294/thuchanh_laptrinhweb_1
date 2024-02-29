4a9479fa23131c72c5adc4c7787836aab1bb3896
maiphuong <121542406+maiphuong294@users.noreply.github.com>
2024-02-29 11:36:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let userInfoForm = document.getElementById('userInfoForm');
+let quizForm = document.getElementById('quizForm');
+let resultDiv = document.getElementById('result');
+
+function startQuiz() {
+    if (!validateForm()){
+        alert('Vui lòng điền đầy đủ thông tin.');
+    }else{
+        userInfoForm.style.display = 'none';
+        quizForm.style.display = 'block';
+    }
+    
+}
+
+function submitQuiz() {
+    const score = calculateScore();
+
+    quizForm.style.display = 'none';
+    resultDiv.style.display = 'block';
+
+    document.getElementById('score').innerText = `Điểm số của bạn: ${score}/10`;
+}
+
+function calculateScore() {
+
+    return Math.floor(Math.random() * 10);
+}
+function validateForm() {
+    var fullName = document.getElementById('fullName').value;
+    var dob = document.getElementById('dob').value;
+    var studentID = document.getElementById('studentID').value;
+    var classValue = document.getElementById('class').value;
+
+    if (fullName === '' || dob === '' || studentID === '' || classValue === '') {
+        
+        return false;
+    }
+    return true;
+}
